42cd393406cf341e3d0802ba777cc3575377b2b5
Albin <albiv.forsberg2@elev.ga.ntig.se>
2024-03-20 10:16:31 +0100

    fyjjfy

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+if (6 >= 3){
+    Console.WriteLine("Hellwo world");
+}
+
+
+
+
+// Console.WriteLine("Vad heter du?");
+
+// string namn = Console.ReadLine();
+
+// if (namn == "noname")
+// {
+//     Console.WriteLine("Välkommen");
+// }
+
+
+
+Console.ReadLine();
